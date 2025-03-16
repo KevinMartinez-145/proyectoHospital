8f4233655ce0ecf4cf7d2b30152efe63b18f560d
KevinMartinez <callofdutyblacfff@gmail.com>
2025-03-16 00:37:47 -0600

    carpetas de utilidades

--- src/componentes/boton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Boton = ({ texto, onClick }) => {
-  return <button onClick={onClick}>{texto}</button>;
-};
-
-export default Boton;
--- src/utilidades/ValidarPaciente.js
+++
@@ -1,6 +0,0 @@
-export const validarPaciente = (paciente) => {
-    if (!paciente.nombre || paciente.edad <= 0) {
-      return false;
-    }
-    return true;
-  };
